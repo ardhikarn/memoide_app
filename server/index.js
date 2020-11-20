e204dc7997d74fba62d9cc48c87c7d79f33277da
Ardhika Ryzha Nurmawan <62016621+ardhikarn@users.noreply.github.com>
2020-11-20 17:11:38 +0700

    add env and git

--- server/index.js
+++ server/index.js
@@ -14,6 +14,11 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes); // harus dibawah cors
+
+app.get("/", (req, res) => {
+  res.send("Hello to Memoide API");
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
