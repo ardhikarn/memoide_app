6921a54d2428a3e9f8e57cde98e1f8f8aa5f3ed1
Ardhika Ryzha Nurmawan <62016621+ardhikarn@users.noreply.github.com>
2020-11-20 16:18:48 +0700

    add env

--- server/index.js
+++ server/index.js
@@ -2,22 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes); // harus dibawah cors
-const CONNECTION_URL =
-  "mongodb+srv://memoide:Indonesia3737@cluster0.pymgz.mongodb.net/memoide";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
